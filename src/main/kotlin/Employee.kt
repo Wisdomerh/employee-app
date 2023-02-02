a56a23a6cea9c673d75aed41741696df6ca56287
Wisdom Erhimwionsobo <20097898@mail.wit.ie>
2023-02-02 16:11:14 +0000

    updating employee class

--- src/main/kotlin/Employee.kt
+++ src/main/kotlin/Employee.kt
@@ -1,5 +1,5 @@
 package ie.setu
 
-class Employee (var firstname: String, var surname: String, var gender: Char, var employeeID: Int,
-    var grossSalary: Double, var payePercentage: Double, var prsiPercentage: Double,
-    var annualBonus: Double, var cycleToWorkDeduction: Double)
+class Employee (var firstName: String, var surname: String, var gender: Char, var employeeID: Int,
+                var grossSalary: Double, var payePercentage: Double, var prsiPercentage: Double,
+                var annualBonus: Double, var cycleToWorkMonthlyDeduction: Double)
