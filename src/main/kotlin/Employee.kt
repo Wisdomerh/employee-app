e5ec5b1044b02f6c03eb1e18da322dcf0be8a824
Wisdom Erhimwionsobo <20097898@mail.wit.ie>
2023-02-02 15:54:55 +0000

    adding employee class

---
+++ src/main/kotlin/Employee.kt
@@ -0,0 +1,5 @@
+package ie.setu
+
+class Employee (var firstname: String, var surname: String, var gender: Char, var employeeID: Int,
+    var grossSalary: Double, var payePercentage: Double, var prsiPercentage: Double,
+    var annualBonus: Double, var cycleToWorkDeduction: Double)
---
+++ src/main/kotlin/ie.setu.main.kt
@@ -0,0 +1,86 @@
+
+package ie.setu
+
+import kotlin.math.round
+
+val firstName = "Joe"
+val surname = "Soap"
+val gender = 'm'
+val employeeID = 6143
+val grossSalary = 67543.21
+val payePercentage = 38.5
+val prsiPercentage = 5.2
+val annualBonus = 1450.50
+val cycleToWorkMonthlyDeduction = 54.33
+
+fun main(args: Array<String>){
+
+    var input : Int
+
+    do {
+        input = menu()
+        when(input) {
+            1 -> println("Monthly Salary: ${getMonthlySalary()}")
+            2 -> println("Monthly PRSI: ${getMonthlyPRSI()}")
+            3 ->println("Monthly PAYE: ${getMonthlyPAYE()}")
+            4 -> println("Monthly Gross Pay: ${getGrossMonthlyPay()}")
+            5 -> println("Monthly Total Deductions: ${getTotalMonthlyDeductions()}")
+            6 -> println("Monthly Net Pay: ${getNetMonthlyPay()}")
+            7 -> println(getPayslip())
+            -1 -> println("Exiting App")
+            else -> println("Invalid Option")
+        }
+        println()
+    } while (input != -1)
+}
+
+fun menu() : Int {
+    print("""
+         Employee Menu for ${getFullName()}
+           1. Monthly Salary
+           2. Monthly PRSI
+           3. Monthly PAYE
+           4. Monthly Gross Pay
+           5. Monthly Total Deductions
+           6. Monthly Net Pay
+           7. Full Payslip
+          -1. Exit
+         Enter Option : """)
+    return readLine()!!.toInt()
+}
+
+fun getFullName() = when (gender){
+    'm', 'M' -> "Mr. $firstName $surname"
+    'f', 'F' -> "Ms. $firstName $surname"
+    else -> "$firstName $surname"
+}
+
+fun getMonthlySalary() = roundTwoDecimals(grossSalary / 12)
+fun getMonthlyPRSI() = roundTwoDecimals(getMonthlySalary() * (prsiPercentage / 100))
+fun getMonthlyPAYE() = roundTwoDecimals(getMonthlySalary() * (payePercentage / 100))
+fun getGrossMonthlyPay() = roundTwoDecimals(getMonthlySalary() + (annualBonus / 12))
+fun getTotalMonthlyDeductions() = roundTwoDecimals((getMonthlyPRSI() + getMonthlyPAYE() + cycleToWorkMonthlyDeduction))
+fun getNetMonthlyPay() = roundTwoDecimals(roundTwoDecimals(getGrossMonthlyPay() - getTotalMonthlyDeductions()))
+
+fun getPayslip() =
+    """
+        ______________________________________________________________________
+         Monthly Payslip:             ${getFullName()}, ID: $employeeID                  
+        ______________________________________________________________________    
+              PAYMENT DETAILS (gross pay: ${getGrossMonthlyPay()})                                                                    
+        ______________________________________________________________________
+                   Salary: ${getMonthlySalary()}
+                   Bonus:  ${roundTwoDecimals(annualBonus / 12)}            
+        ______________________________________________________________________
+              DEDUCTION DETAILS (total Deductions: ${getTotalMonthlyDeductions()})      
+        ______________________________________________________________________
+                   PAYE: ${getMonthlyPAYE()}                
+                   PRSI: ${getMonthlyPRSI()}  
+                   Cycle To Work: $cycleToWorkMonthlyDeduction         
+        ______________________________________________________________________
+             NET PAY: ${getNetMonthlyPay()} 
+        ______________________________________________________________________"""
+
+
+fun roundTwoDecimals(number: Double) = round(number * 100) / 100
+
